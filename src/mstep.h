fec0316cfbcf0e3f10f2032ad45ea95bf30f6108
Youngsun Kim <32813893+kim0sun@users.noreply.github.com>
2023-09-18 20:58:24 +0900

    Update mstep.h

--- src/mstep.h
+++ src/mstep.h
@@ -4,6 +4,7 @@
 #include <Rcpp.h>
 
 // M-step
+void cumPi(double *numer, double *denom, double *post, int nobs, int nclass);
 void cumTau(
    double *numer, double *denom, double *joint, int nobs, int nk, int nl);
 void cumRho(
@@ -11,7 +12,7 @@ void cumRho(
    int *y, int nobs, int nvar, int *ncat,
    int nk, double *post, const double *old_rho
 );
-void updatePi(double *pi, double *post, int nobs, int nclass);
+void updatePi(double *pi, double *numer, double *denom, int nclass);
 void updateTau(double *tau, double *numer, double *denom, int nk, int nl, int* restr);
 void updateRho(
    double *rho, double *numer, double *denom,
