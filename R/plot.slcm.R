e88798feb1399e3d4618dbca217f3452871b64ad
kim0sun <kim0sun@korea.ac.kr>
2023-09-24 20:20:31 +0900

    plot bug fix

--- R/plot.slcm.R
+++ R/plot.slcm.R
@@ -5,25 +5,26 @@ plot.slcm <- function(x, abbreviation = FALSE, dir = "TD",
    tree <- x$model$tree
 
    latent$child <- NA
-   latent$child[latent$leaf] <- if (abbreviation)
-      sapply(latent$children[latent$leaf], function(x)
-         paste0("'", x[1], " ~ ", x[length(x)], "'")) else
-            sapply(latent$children[latent$leaf], function(x)
-               paste0("'", x, "'", collapse = ", "))
-   latent$child[!latent$leaf] <-
-      sapply(latent$children[!latent$leaf], function(x)
-         paste0("'", x, "'", collapse = ", "))
+   if (abbreviation)
+      latent$children[latent$leaf] <-
+         sapply(latent$children[latent$leaf], function(x)
+            paste0("'", x[1], " ~ ", x[length(x)], "'"))
+
+   parent <- row.names(latent)
+   child <- sapply(latent$children, paste, collapse = ", ")
 
    node <- paste0(
       "node [shape = box]\n",
-      paste(tree$child[tree$leaf], collapse = ", "),
+      paste(child[latent$leaf], collapse = ", "),
       "\n\n node [shape = oval]\n",
-      paste(rownames(latent), collapse = ", ")
+      paste(names(child), collapse = ", ")
    )
-   path <- paste(paste(rownames(latent), " -> {", latent$child, "}"), collapse = "\n")
+   path <- paste(paste(names(child), " -> {", child, "}"), collapse = "\n")
    if (!missing(equal_rank)) {
       equal_rank <- equal_rank[equal_rank %in% row.names(latent)]
-      ranks <- paste0("{rank = same; '", paste0(equal_rank, collapse = "';'"), "';}")
+      ranks <- paste0(
+         "{rank = same; '", paste0(equal_rank, collapse = "';'"), "';}"
+      )
    }
 
    text <- paste0(
